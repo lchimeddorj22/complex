dae40b49c7762232b2a6d90069dfbe912dd3ae96
lutaa <lchimeddorj22@gmail.com>
2022-05-05 16:14:28 +0800

    Travis CI

--- client/client/server/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    pgUser: process.env.PGUSER,
-    pgHOST: process.env.PGHOST,
-    pgDATABASE: process.env.PGDATABASE,
-    pgPASSWORD: process.env.PGPASSWORD,
-    pgPORT: process.env.PGPORT,
-};
